986ee72e61f2044eb64ba128469380d642185e28
Felipe Martinez <pipernene@gmail.com>
2020-04-03 12:44:44 +0200

    Add benchmarks

---
+++ Tester/Benchmarks.cs
@@ -0,0 +1,72 @@
+﻿using BenchmarkDotNet.Attributes;
+using LogicScript;
+using LogicScript.Data;
+using LogicScript.Parsing.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class Benchmarks
+    {
+        private const string RawScript =
+            @"
+when (in[1], in[2]) = 11
+    out = 13'
+    out = add(10', 4')
+    out = add(10, 01)
+    out = and(10, 01)
+    out = or(10, 01)
+
+    out[0] = 1
+    out[0] = and(1, 1)
+    out[0] = or(1, 1)
+end
+";
+
+        private Script Compiled;
+        private LogicRunner Runner;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Compiled = Script.Compile(RawScript).Script;
+            Runner = new LogicRunner(Compiled);
+        }
+
+        [Benchmark]
+        public void CompileScript()
+        {
+            Script.Compile(RawScript);
+        }
+
+        [Benchmark]
+        public void RunScript()
+        {
+            Runner.DoUpdate(NoopMachine.Instance);
+        }
+
+        private class NoopMachine : IMachine
+        {
+            public static NoopMachine Instance { get; } = new NoopMachine();
+
+            public int InputCount { get; } = 5;
+            public int OutputCount { get; } = 5;
+
+            public bool GetInput(int i) => true;
+
+            public BitsValue GetInputs() => 0;
+
+            public void SetOutput(int i, bool on)
+            {
+            }
+
+            public void SetOutputs(BitsValue values)
+            {
+            }
+        }
+    }
+}
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,4 +1,7 @@
-﻿using LogicScript;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+using LogicScript;
 using LogicScript.Data;
 using LogicScript.Parsing;
 using LogicScript.Parsing.Structures;
@@ -13,22 +16,18 @@ namespace Tester
     {
         static void Main(string[] args)
         {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmarks>(ManualConfig.Create(DefaultConfig.Instance).With(MemoryDiagnoser.Default));
+#else
+
             const string script = @"
 when 0 = 0
-    # Set individual output bits
     out[2] = and(1, 1)
     out[2] = and(1, in[2])
 
-    # Set all the output bits
     out = 1010
     out = add(10', 3')
 end
-
-# Other example case statements:
-# when in = (1, 0, in[1], 1)
-# when in = 12'
-# when (in[0], in[1]) = 10
-# when (in[2], in[1]) = 3'
 ";
 
             var result = Script.Compile(script);
@@ -47,22 +46,8 @@ end
             var machine = new Machine();
             engine.DoUpdate(machine);
 
-#if RELEASE
-            machine.ConsoleOutput = false;
-
-            const int iterations = 10000000;
-            var sw = Stopwatch.StartNew();
-
-            for (int i = 0; i < iterations; i++)
-            {
-                engine.DoUpdate(machine);
-            }
-
-            sw.Stop();
-            Console.WriteLine((double)(sw.ElapsedTicks / iterations) / TimeSpan.TicksPerMillisecond + "ms per iteration");
-#endif
-
             Console.ReadKey(true);
+#endif
         }
     }
 
