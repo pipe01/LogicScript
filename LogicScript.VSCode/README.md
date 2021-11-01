d4712164361d9dafab78120096b2e73fe2f40627
Felipe Martinez <pipernene@gmail.com>
2021-11-01 12:44:54 +0100

    Move VS Code extension to main repo

---
+++ LogicScript.VSCode/README.md
@@ -0,0 +1,3 @@
+# logicscript-lang
+
+Provides syntax highlighting for the LogicScript language in VS Code.
---
+++ LogicScript.VSCode/src/extension.ts
@@ -0,0 +1,56 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// const serverPath = context.asAbsolutePath(
+	// 	path.join('server', 'out', 'server.js')
+	// );
+    const serverPath = "C:\\Users\\piper\\source\\repos\\LogicScript\\LogicScript.LSP\\bin\\Debug\\netcoreapp3.1\\LogicScript.LSP.exe";
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: {
+            command: serverPath,
+            transport: TransportKind.stdio,
+        },
+        debug: {
+            command: "dotnet",
+            args: ["run", "--project", "..\\LogicScript.LSP\\LogicScript.LSP.csproj"],
+            transport: TransportKind.stdio,
+        }
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'logicscript' }]
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'logicscript',
+		'LogicScript',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
