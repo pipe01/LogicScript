0491c2412052660bd0a25a57b55cb2586ffc9552
Felipe Martinez <pipernene@gmail.com>
2021-11-02 18:04:02 +0100

    Fail if publish fails

--- LogicScript.VSCode/.scripts/publish.js
+++ LogicScript.VSCode/.scripts/publish.js
@@ -6,3 +6,5 @@ const fileArgs = readdirSync("vsix").map(o => `-i "vsix/${o}"`).join(" ");
 const proc = exec(`vsce publish ${fileArgs}`);
 proc.stdout.pipe(process.stdout);
 proc.stderr.pipe(process.stderr);
+
+proc.addListener("exit", code => process.exit(code));
