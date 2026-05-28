98be9b06853252f986928c9624162fb934a9e3ee
Felipe Martinez <felipe@pipe01.net>
2026-05-28 14:44:05 +0200

    Rate limit document parsing

--- LogicScript.DX.LSP/Handlers/DocumentHandler.cs
+++ LogicScript.DX.LSP/Handlers/DocumentHandler.cs
@@ -6,16 +6,21 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ThrottleDebounce;
 
 namespace LogicScript.DX.LSP.Handlers
 {
-    class DocumentHandler(ILanguageServerFacade server, Workspace workspace) : IDidChangeTextDocumentHandler, IDidOpenTextDocumentHandler
+    class DocumentHandler(ILanguageServerFacade server, Workspace workspace) : IDidChangeTextDocumentHandler, IDidOpenTextDocumentHandler, IDidCloseTextDocumentHandler
     {
         private readonly ILanguageServerFacade Server = server;
         private readonly Workspace Workspace = workspace;
+
+        private readonly Dictionary<DocumentUri, RateLimitedAction<string>> SendDiagnosticsThrottled = [];
 
         TextDocumentOpenRegistrationOptions IRegistration<TextDocumentOpenRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
         {
@@ -34,27 +39,70 @@ namespace LogicScript.DX.LSP.Handlers
             };
         }
 
+        TextDocumentCloseRegistrationOptions IRegistration<TextDocumentCloseRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new()
+            {
+                DocumentSelector = Program.Selector
+            };
+        }
+
         public Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
         {
-            SendDiagnostics(request.TextDocument.Text, request.TextDocument.Uri);
+            ThrottlerFor(request.TextDocument.Uri).Invoke(request.TextDocument.Text);
             return Unit.Task;
         }
 
         public Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
         {
-            SendDiagnostics(request.ContentChanges.First().Text, request.TextDocument.Uri);
+            ThrottlerFor(request.TextDocument.Uri).Invoke(request.ContentChanges.First().Text);
             return Unit.Task;
         }
 
-        private void SendDiagnostics(string script, DocumentUri uri)
+        public Task<Unit> Handle(DidCloseTextDocumentParams request, CancellationToken cancellationToken)
         {
-            var diag = Workspace.LoadScript(uri, script);
+            if (SendDiagnosticsThrottled.TryGetValue(request.TextDocument.Uri, out var throttler))
+            {
+                throttler.Dispose();
+                SendDiagnosticsThrottled.Remove(request.TextDocument.Uri);
+            }
+
+            return Unit.Task;
+        }
+
+        private void SendDiagnostics(string source, DocumentUri uri)
+        {
+            Debug.WriteLine($"Updated script at {uri}");
+
+            var diag = Workspace.LoadScript(uri, source, out var script);
 
             Server.Client.SendNotification(new PublishDiagnosticsParams
             {
-                Diagnostics = Container<Diagnostic>.From(diag ?? Array.Empty<Diagnostic>()),
+                Diagnostics = new(diag ?? []),
                 Uri = uri
             });
+
+            if (script != null)
+            {
+                Server.SendNotification("logicscript/foundTests", new
+                {
+                    uri,
+                    tests = script.TestCases.Select(t => new
+                    {
+                        id = HashCode.Combine(t.Index, t.Name).ToString(),
+                        name = t.Name ?? $"Case {t.Index}",
+                        range = t.Span.ToRange(),
+                    }),
+                });
+            }
+        }
+
+        private RateLimitedAction<string> ThrottlerFor(DocumentUri uri)
+        {
+            if (!SendDiagnosticsThrottled.TryGetValue(uri, out var throttler))
+                SendDiagnosticsThrottled[uri] = throttler = Throttler.Throttle<string>(source => SendDiagnostics(source, uri), TimeSpan.FromSeconds(1));
+
+            return throttler;
         }
     }
 }
